3aceed458a0545c6f254e267f722e6999a22230f
Ash@metaschool <ash20pk@gmail.com>
2024-07-18 01:42:57 +0800

    removed extra files

--- interface/src/utils/stringify.ts
+++
@@ -1,8 +0,0 @@
-export const stringify: typeof JSON.stringify = (value, replacer, space) =>
-  JSON.stringify(
-    value,
-    (key, value_) => {
-      return typeof replacer === 'function' ? replacer(key, value_) : value_
-    },
-    space,
-  )
